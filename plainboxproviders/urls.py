b9290733998cb34ef8974b37ccc052ef57c13f8a
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-07-15 16:27:12 +0200

    set Home view as the main view of the project
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- plainboxproviders/urls.py
+++ plainboxproviders/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from providerfrontend.views import Home
+
 urlpatterns = patterns('',
+    url(r'^$', Home.as_view(), name='home'),
     # Examples:
     # url(r'^$', 'plainboxproviders.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
