19914b285aa15674d7233b83f1feddaeef634e77
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-08-08 19:14:06 +0200

    Optionally wrap wsgi app with Cling
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- plainboxproviders/wsgi.py
+++ plainboxproviders/wsgi.py
@@ -12,3 +12,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plainboxproviders.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+try:
+    from dj_static import Cling
+except ImportError:
+    pass
+else:
+    application = Cling(application)
