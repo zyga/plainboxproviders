1c75e7e2c5e2f7980c10b562444df8da047693f7
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-08-08 19:18:30 +0200

    Add recommended Heroku settings
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- plainboxproviders/settings.py
+++ plainboxproviders/settings.py
@@ -97,3 +97,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
